40fdc0b8805511dcac1b57b362bf14f6a3431f82
MiguelTAR <mtarazona4489@conestogac.on.ca>
2026-03-29 19:09:01 -0400

    Creating the base for the fucntions and starting a while loop.

--- Listing.c
+++ Listing.c
@@ -1,4 +1,20 @@
 #include <string.h>
 #include "listing.h"
+#include <stdio.h>
 
-static listing
+int loadListings(const char* filename, Listing listings[], int maxListings) {
+	FILE* file = fopen(filename, "r");
+	if (!file) return -1;
+
+	char line[256];
+	int count = 0;
+	double rent = -1;
+	char type[TYPE_LEN] = "";
+	int bedrooms = -1;
+	char location[LOCATION_LEN] = "";
+
+	while (fgets(line, sizeof(line), file) && count < maxListings) {
+
+	}
+
+}
