8f2d5413d56dedfde80611507969073ca3da72c5
MiguelTAR <mtarazona4489@conestogac.on.ca>
2026-04-01 12:47:48 -0400

    Creating function load listings. The main purpose of the fucntion is to read all readings from list.txt, orgainze the four elements of rent, tpye, bedrooms, locations, and store it. along with an ID.

--- Listing.c
+++ Listing.c
@@ -2,7 +2,9 @@
 #include "listing.h"
 #include <stdio.h>
 
-int loadListings(const char* filename, Listing listings[], int maxListings) {
+#define listing_file "list.txt"
+//Creating a function to load the listings from the text file.
+int loadListings(const char* filename, Listing listing[], int maxListing) {
 	FILE* file = fopen(filename, "r");
 	if (!file) return -1;
 
@@ -13,8 +15,37 @@ int loadListings(const char* filename, Listing listings[], int maxListings) {
 	int bedrooms = -1;
 	char location[LOCATION_LEN] = "";
 
-	while (fgets(line, sizeof(line), file) && count < maxListings) {
+	while (fgets(line, sizeof(line), file) && count < maxListing) {
+		
+		if (strncmp(line, "Monthly Rent: ", 13) == 0) {
+			sscanf(line, "Monthly Rent: ", &rent);
+		}
+		else if (strncmp(line, "Type of housing: ", 16) == 0) {
+			sscanf(line, "Type of housing: %[^\n]", type);
+		}
+		else if (strcmp(line, "bedrooms: ", 9) == 0) {
+			sscanf(line, "bedrooms: %d", &bedrooms);
+		}
+		else if (strncmp(line, "Location: ", 9) == 0) {
+			sscanf(line, "Location: %[^\n]", location);
+		}
 
-	}
+		//When all four fields are collected, store the listing.
+		if (rent > 0 && strlen(type) > 0 && bedrooms >= 0 && strlen(location) > 0) {
+			listing[count].id = count + 1;
+			listing[count].monthlyRent = rent;
+
+			strcpy(listing[count].Type, type, TYPE_LEN - 1);
+			listing[count].Type[TYPE_LEN - 1] = '\0';
+
+			listing[count].bedrooms = bedrooms;
+
+			strncpy(listing[count].location, location, LOCATION_LEN - 1);
+			listing[count].location[LOCATION_LEN - 1] = '\0';
+
+			count++;
+
+
 
 }
+
