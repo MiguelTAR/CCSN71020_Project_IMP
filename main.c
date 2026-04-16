e27add1ba08c894b65c989ebeba5509114fc8d28
JeanPaulPE <Jangelesluza7933@conestogac.on.ca>
2026-04-16 16:56:21 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,66 +2,6 @@
 #include "main.h"
 #include "user.h"
 #include "user.c"
-
-
-void userRegistration();
-void userLogin();
-
-//definition of the dummyfunction. You can remove this function once the project works.
-int dummyFunction(int x, int y) {
-	return x + y;
-}
-
-//definition of functions here. E.g., 
-// int yourFunction(...){
-//		return INVALID_VALUE;
-// }
-
-
-
-
-int main() {
-	
-	userRegistration();
-	userLogin();
-	
-	int option;
-
-	while (1) {
-		printf("                          \n");
-		printf("        Menu Listing      \n");
-		printf("1. Add Listing            \n");
-		printf("2. Delete Listing         \n");
-		printf("3. Add Listing            \n");
-		printf("4. Delete Listing         \n");
-		printf("5. Search Listing         \n");
-		printf("6. View Listing           \n");
-		printf("7. Exit                   \n");
-		printf("                          \n");
-		printf("Enter an option:          \n");
-		scanf_s("%d", &option);
-
-		switch (option) {
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-		case 5:
-		case 6:
-		case 7: exit();
-			break;
-		}
-	}
-
-	return 0;
-
-}
-
-void exit() {
-	printf("Exit Listing Management System");
-}
-
-#include <stdio.h>
 #include <string.h>
 #include "Listing.h"
 
